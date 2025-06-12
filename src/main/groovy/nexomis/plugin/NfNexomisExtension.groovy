a538a782b7e7e275fcd45aedb05432f84737ecbf
jfouret <julien@fouret.me>
2025-06-12 18:53:25 +0200

    remove show assets help

--- src/main/groovy/nexomis/plugin/NfNexomisExtension.groovy
+++ src/main/groovy/nexomis/plugin/NfNexomisExtension.groovy
@@ -19,34 +19,5 @@ class NfNexomisExtension extends PluginExtensionPoint {
     protected void init(Session session) {
         this.session = session
     }
-    
-    /**
-     * Display help information for all schema files found in the project.
-     * This function discovers schema files referenced in nextflow_schema.json
-     * and displays formatted help for each one.
-     */
-    @Function
-    void showAssetsHelp() {
-        def assetsHelper = new AssetsHelper(this.session.baseDir)
-        
-        // Discover schema files from nextflow_schema.json
-        def schemaFiles = assetsHelper.discoverSchemaFiles()
-        
-  
-        // Process discovered schema files
-        println "\nFound ${schemaFiles.size()} schema file(s) in nextflow_schema.json:"
-        schemaFiles.each { schemaPath ->
-            println "Processing: ${schemaPath}"
-            def helpOutput = assetsHelper.generateSchemaHelp(schemaPath)
-            if (helpOutput) {
-                println helpOutput
-            }
-        }
-        
-        // Add footer
-        def ANSI_GREY = "\u001B[90m"
-        def ANSI_RESET = "\u001B[0m"
-        println "-${ANSI_GREY}----------------------------------------------------${ANSI_RESET}-"
-    }
 
 }
--- src/main/groovy/nexomis/plugin/NfNexomisObserver.groovy
+++ src/main/groovy/nexomis/plugin/NfNexomisObserver.groovy
@@ -4,6 +4,16 @@ import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import nextflow.Session
 import nextflow.trace.TraceObserver
+import nextflow.trace.GraphObserver
+import nextflow.trace.ReportObserver
+import nextflow.trace.TimelineObserver
+import nextflow.trace.TraceFileObserver
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardCopyOption
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Comparator
 
 /**
  * Implements an observer that allows implementing custom
@@ -13,24 +23,48 @@ import nextflow.trace.TraceObserver
 @CompileStatic
 class NfNexomisObserver implements TraceObserver {
 
-    @Override
-    void onFlowCreate(Session session) {
-        
-        println """
-            |            #################################################
-            |            #    _  _                             _         #
-            |            #   | \\| |  ___  __ __  ___   _ __   (_)  __    #
-            |            #   | .` | / -_) \\ \\ / / _ \\ | '  \\  | | (_-<   #
-            |            #   |_|\\_| \\___| /_\\_\\ \\___/ |_|_|_| |_| /__/   #
-            |            #                                               #
-            |            #################################################
-            |            https://github.com/nexomis     https://nexomis.io
-            |"""
-        
-    }
+    Path baseDir
+    Path outputDir
 
     @Override
+    void onFlowCreate(Session session) {
+        println """
+                    #################################################
+                    #    _  _                             _         #
+                    #   | \\| |  ___  __ __  ___   _ __   (_)  __    #
+                    #   | .` | / -_) \\ \\ / / _ \\ | '  \\  | | (_-<   #
+                    #   |_|\\_| \\___| /_\\_\\ \\___/ |_|_|_| |_| /__/   #
+                    #                                               #
+                    #################################################
+                    https://github.com/nexomis     https://nexomis.io
+            """
+        this.baseDir = session.baseDir
+        this.outputDir = session.outputDir
+    }
+    
+    @Override
     void onFlowComplete() {
+        String[] pipeline_info = [
+            GraphObserver.DEF_FILE_NAME,
+            TraceFileObserver.DEF_FILE_NAME,
+            ReportObserver.DEF_FILE_NAME,
+            TimelineObserver.DEF_FILE_NAME
+        ]
+        try {
+            // Define the source pipeline_info directory in baseDir
+
+            Path dirPipelineInfo = outputDir.resolve("pipeline_info")
+            // create dir
+            Files.createDirectories(dirPipelineInfo)
+            // copy pipeline_info to outputDir
+            pipeline_info.each { filename ->
+                Files.copy(baseDir.resolve(filename), dirPipelineInfo.resolve(filename), StandardCopyOption.REPLACE_EXISTING)
+            }
+            
+        } catch (Exception e) {
+            log.error("Error copying pipeline_info directory: ${e.message}", e)
+        }
+        
         println "Pipeline complete! 👋"
     }
 }
